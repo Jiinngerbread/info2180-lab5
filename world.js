5fc885c21d9ae38e1e47f36d936836bdbece94f3
Jiinngerbread <Jiinngerbread@gmail.com>
2020-11-23 00:31:15 -0500

    Added functionality for country button to

---
+++ world.js
@@ -0,0 +1,18 @@
+window.onload = function () {
+    var countryLookupBtn = document.getElementById("lookup");
+    countryLookupBtn.addEventListener("click", function () {
+        let url = "world.php";
+        let country = document.getElementById("country").value;
+       
+        let xhttpRqst = new XMLHttpRequest();
+        //promise not working atm
+        xhttpRqst.onreadystatechange = function () {
+            if (this.readyState === this.DONE && this.status === 200) {
+                let result = document.getElementById("result");
+                result.innerHTML = this.responseText;
+            }
+        };
+        xhttpRqst.open("GET", url + "?country=" + country + "&context="); 
+        xhttpRqst.send();
+    });
+}
